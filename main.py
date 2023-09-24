47f7213fd21334796f18df21f1fbb4defb4f69f5
JaelsonPereira <16573226-JaelsonPereira@users.noreply.replit.com>
2023-09-24 13:25:37 +0000

    organização de menu inicial
    verificado necessidade de organizar para facilitar a escolha

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from menuLista1 import menu_1
 from menuLista2 import menu_2
 
 print('Bem vindo ao S.A.E (SITEMA DE ANALISE DE EXERCICIOS')
-
-while True:
+controle = True
+while controle:
   print('''
-=========================================
-||  1. Entrar 1ª lista de exercicio    ||
-||  2. Entrar 2ª lista de exercicio    ||
-||  0. Para finalizar sistema          ||
-=========================================
+==============================================
+||     1. Modulo 1 Conceito Fundamentais    ||
+||     2. Modulo 2 Introdução a Python      ||
+||     3. Modulo 3 Estrutura de Decisão     ||
+||     4. Modulo 4 Estrutura de Repetição   ||
+||     0. Para finalizar sistema            ||
+==============================================
 ''')
   op = int(input('Escolha a opção desejada:\n'))
 
@@ -17,7 +19,13 @@ while True:
     importar_menu1 = menu_1()
   elif op == 2:
     importal_menu2 = menu_2()
+  elif op == 3:
+    importal_menu2 = menu_2()
+  elif op == 4:
+    importal_menu2 = menu_2()
   elif op == 0:
     print('sistema esta sendo finalizado ...')
-    break
+    controle = False
+  else: 
+    print('Favor escolha uma opção valida')
 print('Tenha um bom dia')
